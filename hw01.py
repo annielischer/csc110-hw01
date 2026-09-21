510fd4249ec5bf179ab735eaf8653c50b02d11f5
annielischer <alischer@smith.edu>
2026-09-21 09:26:27 -0400

    Tests should work now

--- hw01.py
+++ hw01.py
@@ -1,7 +1,7 @@
 # ------------------------------------------------------
-#        Name: (put your name here)
-#       Peers: (add any collaborators)
-#  References: (anything you checked to solve this)
+#        Name: Annie Lischer
+#       Peers: N/A
+#  References: N/A
 # ------------------------------------------------------
 
 
